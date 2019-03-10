af9646bd920bff380446bf4660299b253174dd0f
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-03-10 13:14:44 -0600

    First graphql query with django

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    'links',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'hackernews.schema.schema',
+}
