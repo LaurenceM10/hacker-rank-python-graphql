04118e93973c16d11469b6d7dbf5fcd4f655256a
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-03-10 15:12:34 -0600

    Add: mutation link to schema

--- links/schema.py
+++ links/schema.py
@@ -3,15 +3,45 @@ from graphene_django import DjangoObjectType
 
 from .models import Link
 
-
+# Define the output fields based in Link model
 class LinkType(DjangoObjectType):
     class Meta:
         model = Link
         description = 'This represent a Link'
 
-
+# Query to list links
 class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
 
+    # Return the links from database using Django ORM
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+
+class CreateLink(graphene.Mutation):
+    # Output fields
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    # Mutation arguments
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    # Mutation method: to create a link in the database
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        # Return the link saved, this matches with the output fiels 
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+        )
+
+
+# Mutation class to resolved the CreateLink mutation
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
