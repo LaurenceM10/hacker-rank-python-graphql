4544f0c971d36ddf3254488cbeabc332f49c9717
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-03-10 14:46:14 -0600

    Add: description to LinkType

--- links/schema.py
+++ links/schema.py
@@ -7,6 +7,7 @@ from .models import Link
 class LinkType(DjangoObjectType):
     class Meta:
         model = Link
+        description = 'This represent a Link'
 
 
 class Query(graphene.ObjectType):
